c8ef85a639034be1d78e5a8a422b75b1a20ff650
William Li <williamli@bbi.io>
2020-01-23 16:48:13 +0800

    added text and content changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import styles from './index.css';
 export default function() {
   return (
     <div className={styles.normal}>
+      <h1>THIS IS JUST A TEST... with a cat photo</h1>
+      <img src="https://images.unsplash.com/photo-1498336179775-9836baef8fdf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3450&q=80"/>
       <div className={styles.welcome} />
       <ul className={styles.list}>
         <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
